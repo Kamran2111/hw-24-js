8890a8d4f92ddf520250ff3710c9d3fb361afd06
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-02 17:33:24 +0400

    first commit

---
+++ js/hw-24.js
@@ -0,0 +1,17 @@
+// ЗАДАНИЕ: 1
+// let numArray = [1, 2, 3, 4, 5];
+// function scrollFunc() {
+//   const sum = _.sum(numArray);
+//   console.log(sum);
+// }
+// document.addEventListener("scroll", _.throttle(scrollFunc, 300));
+
+// ЗАДАНИЕ: 2
+// function randomColorFunc() {
+//   const randomColor = `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+//   document.querySelector("body").style.backgroundColor = randomColor;
+// }
+// document.addEventListener("scroll", _.throttle(randomColorFunc, 300));
+// ЗАДАНИЕ: 6
